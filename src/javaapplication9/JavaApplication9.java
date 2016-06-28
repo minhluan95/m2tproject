6df2b21cb3b83028801d327d0b6c2358f0a1f2ae
Administrator <Administrator@THANGLE-PC>
2016-06-28 09:47:32 +0700

    new release

--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -16,7 +16,7 @@ public class JavaApplication9 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ///
+        /// gggg
         ///
         System.out.print("Hello");
     }
